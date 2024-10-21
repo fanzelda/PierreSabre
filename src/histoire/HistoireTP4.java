ade8795fa0203bd2e46e47d3f5194d2ea1678d3d
Gael Jean-albert <jng5087a@ensfsi.onmicrosoft.com>
2024-10-21 18:42:38 +0200

    créationdelaclasseCommercant

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,16 +1,25 @@
 package histoire;
 
 import personnages.Humain;
+import personnages.Commercant;
 
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
-		Humain prof = new Humain("Prof", "kombucha", 54);
+//		Humain prof = new Humain("Prof", "kombucha", 54);
+//		
+//		prof.direBonjour();
+//		String boisson = prof.acheter("kombucha", 12);
+//		prof.boire(boisson);
+//		String jeu = prof.acheter("jeu", 2);
+//		String kimono = prof.acheter("kimono", 50);
 		
-		prof.direBonjour();
-		String boisson = prof.acheter("kombucha", 12);
-		prof.boire(boisson);
-		String jeu = prof.acheter("jeu", 2);
-		String kimono = prof.acheter("kimono", 50);
+		Commercant marco = new Commercant("Marco", 20);
+		
+		marco.direBonjour();
+		marco.seFaireExtorquer();
+		marco.recevoir(15);
+		marco.boire("thé");
+		
 	}
 }
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -19,13 +19,25 @@ public class Humain {
 		this.boissonFavorite = boissonFavorite;
 		this.argent = argent;
 	}
-	
+
+	protected void gagnerArgent(int gain) {
+		argent = argent + gain;
+	}
+
+	protected void perdreArgent(int perte) {
+		argent = argent - perte;
+	}
+
+	protected void parler(String texte) {
+		System.out.println("(prof) - " + texte);
+	}
+
 	public void direBonjour() {
 		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFavorite + ".");
 	}
-	
+
 	public void boire(String boisson) {
-		parler("Mmmm, un bon verre de " + boisson + "! GLOUPS !");
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
 	}
 
 	public String acheter(String bien, int prix) {
@@ -33,26 +45,12 @@ public class Humain {
 			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir une boisson à " + prix + " sous.");
 			argent = argent - prix;
 			return bien;
-		}
-		else {
-			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un " + bien + " à " + prix + " sous.");
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un " + bien + " à "
+					+ prix + " sous.");
 			return "Argent insuffisant";
 		}
-		
+
 	}
-	
-	private void gagnerArgent(int gain) {
-		argent = argent + gain;
-	}
-	
-	private void perdreArgent(int perte) {
-		argent = argent - perte;
-	}
-	
-	private void parler(String texte) {
-		System.out.println("(prof) - " + texte );
-	}
-	
-	
-	
+
 }
